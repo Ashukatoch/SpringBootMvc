19ad11d5c4a8507e0bbc48a917631e72e69f16e8
Ashu Katoch <ashukatoch2017@gmail.com>
2021-07-19 16:06:49 +0530

    First Commit!
    Uploading all the files

---
+++ MyWebApp/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+
+public class HomeController 
+{
+	@RequestMapping("/")
+	public String home()
+	{
+		return "home";
+	}
+	@RequestMapping("/showForm")
+	public String showForm()
+	{
+		return "form";
+	}
+	@RequestMapping("/processForm")
+	public String processForm(HttpServletRequest request,Model model,@RequestParam("StudentName") String name) 
+	{
+		//String name=request.getParameter("StudentName");
+		name=name.toUpperCase();
+		String result="Yo "+name+"!";
+		model.addAttribute("message",result);
+		String m="message";
+		System.out.println(result+" "+model.getAttribute(m));
+	
+		return "display";
+		
+	}
+}
